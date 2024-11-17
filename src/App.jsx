38156e0956bd25d08f3213970481a02f7481865c
Uttkarsh <uttkarshsaini10@gmail.com>
2024-11-17 15:28:12 +0530

    DevTinder UI

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import Body from "./Body";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={ <Login/>} />
+            <Route path="/profile" element={ <Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
